a7f895e2e0819369a28d3e09f021bca240554e7e
Matheus Quiles <matheussquiles@gmail.com>
2025-02-15 11:49:39 -0300

    primeiro commit

---
+++ src/test/java/com/coletas/coletas/ColetasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coletas.coletas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
