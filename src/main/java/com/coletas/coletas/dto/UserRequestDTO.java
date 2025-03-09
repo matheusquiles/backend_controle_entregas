0f729949e309f370d805492c85aeb2151125c918
Matheus Quiles <matheussquiles@gmail.com>
2025-03-09 11:17:51 -0300

    ajuste no cadastro de usuários. Agora há um novo objeto exlusivo para salvar os usuários

---
+++ src/main/java/com/coletas/coletas/dto/UserRequestDTO.java
@@ -0,0 +1,70 @@
+package com.coletas.coletas.dto;
+
+public class UserRequestDTO {
+	
+	private Integer idUser;
+	private String name;
+	private String cpf;
+	private String email;
+	private String userKey;
+	private Boolean status;
+	private Integer userType;
+	private Integer hierarchy;
+	private String password;
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Integer getIdUser() {
+		return idUser;
+	}
+	public void setIdUser(Integer idUser) {
+		this.idUser = idUser;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUserKey() {
+		return userKey;
+	}
+	public void setUserKey(String userKey) {
+		this.userKey = userKey;
+	}
+	public Boolean getStatus() {
+		return status;
+	}
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	public Integer getUserType() {
+		return userType;
+	}
+	public void setUserType(Integer userType) {
+		this.userType = userType;
+	}
+	public Integer getHierarchy() {
+		return hierarchy;
+	}
+	public void setHierarchy(Integer hierarchy) {
+		this.hierarchy = hierarchy;
+	}
+
+}
